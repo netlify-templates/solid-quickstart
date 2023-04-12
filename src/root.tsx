c8d582e97857a2d121e518336b51c70388e9eba3
IKUN-114 <130162337+IKUN-114@users.noreply.github.com>
2023-04-12 21:19:49 +0800

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -25,7 +25,7 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
+            <A href="/">chei</A>
             <A href="/about">About</A>
             <Routes>
               <FileRoutes />
