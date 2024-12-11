2521dd46c8ce570b77063c27ce678894ea63e004
TomasBankauskas <tbdizainas@gmail.com>
2024-12-11 16:37:47 +0200

    updated project to new version

---
+++ src/app.tsx
@@ -0,0 +1,47 @@
+import { Suspense, type Component } from 'solid-js';
+import { A, useLocation } from '@solidjs/router';
+
+const App: Component = (props: { children: Element }) => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <nav class="bg-slate-100 text-slate-900 px-4">
+        <ul class="flex items-center">
+          <li class="py-2 px-4">
+            <A href="/" class="no-underline hover:underline">
+              Home
+            </A>
+          </li>
+          <li class="py-2 px-4">
+            <A href="/about" class="no-underline hover:underline">
+              About
+            </A>
+          </li>
+          <li class="py-2 px-4">
+            <A href="/error" class="no-underline hover:underline">
+              Error
+            </A>
+          </li>
+
+          <li class="text-sm flex items-center space-x-1 ml-auto">
+            <span>URL:</span>
+            <input
+              class="w-75px p-1 bg-white text-sm rounded-lg"
+              type="text"
+              readOnly
+              value={location.pathname}
+            />
+          </li>
+        </ul>
+      </nav>
+
+      <main class="max-w-2xl mx-auto h-screen flex flex-col justify-center px-4">
+        <h1 class="text-5xl mx-auto mb-8 text-slate-600 font-light">Welcome to my Solid app!</h1>
+        <Suspense>{props.children}</Suspense>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/errors/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <section class="bg-red-100 text-slate-700 p-8 rounded-md">
+      <h2 class="text-2xl">404: Not Found</h2>
+      <p class="mt-4">It's gone 😞!</p>
+    </section>
+  );
+}
